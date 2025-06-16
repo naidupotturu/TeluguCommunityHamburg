aee48cad1fc9756fd99df83851468d9278caf585
vanganur <bharathi.vanganuru@xfel.eu>
2025-06-16 23:42:13 +0200

    added tabs and home

--- config/routes.rb
+++ config/routes.rb
@@ -4,19 +4,27 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   # get "up" => "rails/health#show", as: :rails_health_check
-    root 'users#index'  # Web homepage
+    root 'home#index'  # Web homepage
     resources :users, only: [:index, :update, :edit, :show]
 
-    resources :films, only: [:index]
-    resources :payments, only: [:index]
-    get 'charts', to: 'films#dashboard'
+    # resources :films, only: [:index]
+    # resources :payments, only: [:index]
+    # get 'charts', to: 'films#dashboard'
 
-    namespace :api do
-      resources :users, only: [:index, :update]
-      resources :films, only: [:index]
-      resources :payments, only: [:index]
-
-    end
+    # namespace :api do
+    #   resources :users, only: [:index, :update]
+    #   resources :films, only: [:index]
+    #   resources :payments, only: [:index]
+    # end
+    get '/about',       to: 'tabs#about'
+    get '/about',       to: 'tabs#about'
+    get '/membership',  to: 'tabs#membership'
+    get '/gallery',     to: 'tabs#gallery'
+    get '/events',      to: 'tabs#events'
+    get '/calendar',    to: 'tabs#calendar'
+    get '/contact',     to: 'tabs#contact'
+    get '/leadership',  to: 'tabs#leadership'
+    get '/login',       to: 'sessions#new'
 
 
 end
